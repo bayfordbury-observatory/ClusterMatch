37f0121dcc48cc8fb35d27a2e8e624d2636c3a89
bayfordbury-observatory <50171257+bayfordbury-observatory@users.noreply.github.com>
2021-07-27 17:22:16 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,8 +6,8 @@ arr = np.genfromtxt(sys.argv[1:][0], delimiter=',')
 
 arr_T = arr.T
 
-plt.scatter(arr_T[4], arr_T[2], c=0-arr_T[4], cmap='inferno', s=20)
-plt.clim(-3,0) 
+plt.scatter(arr_T[4], arr_T[2], c=0-arr_T[4], cmap='RdYlBu', s=30,  edgecolors='black')
+plt.clim(-2,2) 
 plt.gca().invert_yaxis()
 plt.xlabel('B-V')
 plt.ylabel('V magnitude')
