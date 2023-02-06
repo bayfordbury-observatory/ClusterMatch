e3351696d9ff65a0f8fd4b676ae2c4f3a217bfe1
bayfordbury-observatory <50171257+bayfordbury-observatory@users.noreply.github.com>
2023-02-06 16:15:14 +0000

    Update download.py
    Update for version change with Vizier

--- download.py
+++ download.py
@@ -17,16 +17,18 @@ print("RA: "+str(RA));
 DEC= float(hdul[0].header['CRVAL2'])	
 print("DEC: "+str(DEC));
 
-#Create post data payload (input coordinates, output format.. etc)
-postData = {  '//source' : 'I/322', '//tables' : 'I/322A/out', '-out.max' : '10000',  '-out.form' : '| -Separated-Values',  '-oc.form' : 'dec',  '-c.eq' : 'J2000', '-c.r' : '++2', '-c.u' : 'arcmin', '-c.geom' : 'r', '-out.src' : 'I/322A/out', '-source' : 'I/322A/out', '-out.orig' : 'standard',  '-out' : 'RAJ2000, DEJ2000, Vmag, Bmag', 'Bmag' : '>0',  'Vmag' : '>0', 'RAJ2000' : str(RA)+'+/-0.5',  'DEJ2000' : str(DEC)+'+/-0.5','-bmark' : 'POST'}
+
+postData = {  '-source' : 'I/322A/out', '-out.max' : '10000',  '-out.form' : '| -Separated-Values',  '-oc.form' : 'dec',  '-c.eq' : 'J2000', '-c.r' : '++2', '-c.u' : 'arcmin', '-c.geom' : 'r', '-out.src' : 'I/322A/out', '-source' : 'I/322A/out', '-out.orig' : 'standard',  '-out' : 'RAJ2000, DEJ2000, Vmag, Bmag', 'Bmag' : '>0',  'Vmag' : '>0', 'RAJ2000' : str(RA)+'+/-0.5',  'DEJ2000' : str(DEC)+'+/-0.5'}
 
 #URL encode data
+
 data = parse.urlencode(postData).encode()
 
-#Create web request
-req =  request.Request('https://vizier.u-strasbg.fr/viz-bin/asu-tsv', data=data)
 
-print("Downloading UCAC4 data")
+#Create web request
+req =  request.Request('http://vizier.u-strasbg.fr/viz-bin/asu-tsv', data=data)
+
+#print("Downloading UCAC4 data")
 
 #Request web query
 response = request.urlopen(req)
